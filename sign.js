d30a39db81570b640af6270c8ef5c6958794992d
AttiyaaHassann <aeeaiyaa@gmail.com>
2026-05-05 21:02:16 +0500

    refactor: modularize core responsibilities

--- sign.js
+++ sign.js
@@ -1,87 +1,20 @@
 const timespan = require('./lib/timespan');
-const PS_SUPPORTED = require('./lib/psSupported');
 const validateAsymmetricKey = require('./lib/validateAsymmetricKey');
 const jws = require('jws');
-const includes = require('lodash.includes');
-const isBoolean = require('lodash.isboolean');
-const isInteger = require('lodash.isinteger');
-const isNumber = require('lodash.isnumber');
-const isPlainObject = require('lodash.isplainobject');
 const isString = require('lodash.isstring');
 const once = require('lodash.once');
-const { KeyObject, createSecretKey, createPrivateKey } = require('crypto')
-
-const SUPPORTED_ALGS = ['RS256', 'RS384', 'RS512', 'ES256', 'ES384', 'ES512', 'HS256', 'HS384', 'HS512', 'none'];
-if (PS_SUPPORTED) {
-  SUPPORTED_ALGS.splice(3, 0, 'PS256', 'PS384', 'PS512');
-}
-
-const sign_options_schema = {
-  expiresIn: { isValid: function(value) { return isInteger(value) || (isString(value) && value); }, message: '"expiresIn" should be a number of seconds or string representing a timespan' },
-  notBefore: { isValid: function(value) { return isInteger(value) || (isString(value) && value); }, message: '"notBefore" should be a number of seconds or string representing a timespan' },
-  audience: { isValid: function(value) { return isString(value) || Array.isArray(value); }, message: '"audience" must be a string or array' },
-  algorithm: { isValid: includes.bind(null, SUPPORTED_ALGS), message: '"algorithm" must be a valid string enum value' },
-  header: { isValid: isPlainObject, message: '"header" must be an object' },
-  encoding: { isValid: isString, message: '"encoding" must be a string' },
-  issuer: { isValid: isString, message: '"issuer" must be a string' },
-  subject: { isValid: isString, message: '"subject" must be a string' },
-  jwtid: { isValid: isString, message: '"jwtid" must be a string' },
-  noTimestamp: { isValid: isBoolean, message: '"noTimestamp" must be a boolean' },
-  keyid: { isValid: isString, message: '"keyid" must be a string' },
-  mutatePayload: { isValid: isBoolean, message: '"mutatePayload" must be a boolean' },
-  allowInsecureKeySizes: { isValid: isBoolean, message: '"allowInsecureKeySizes" must be a boolean'},
-  allowInvalidAsymmetricKeyTypes: { isValid: isBoolean, message: '"allowInvalidAsymmetricKeyTypes" must be a boolean'}
-};
-
-const registered_claims_schema = {
-  iat: { isValid: isNumber, message: '"iat" should be a number of seconds' },
-  exp: { isValid: isNumber, message: '"exp" should be a number of seconds' },
-  nbf: { isValid: isNumber, message: '"nbf" should be a number of seconds' }
-};
-
-function validate(schema, allowUnknown, object, parameterName) {
-  if (!isPlainObject(object)) {
-    throw new Error('Expected "' + parameterName + '" to be a plain object.');
-  }
-  Object.keys(object)
-    .forEach(function(key) {
-      const validator = schema[key];
-      if (!validator) {
-        if (!allowUnknown) {
-          throw new Error('"' + key + '" is not allowed in "' + parameterName + '"');
-        }
-        return;
-      }
-      if (!validator.isValid(object[key])) {
-        throw new Error(validator.message);
-      }
-    });
-}
-
-function validateOptions(options) {
-  return validate(sign_options_schema, false, options, 'options');
-}
-
-function validatePayload(payload) {
-  return validate(registered_claims_schema, true, payload, 'payload');
-}
-
-const options_to_payload = {
-  'audience': 'aud',
-  'issuer': 'iss',
-  'subject': 'sub',
-  'jwtid': 'jti'
-};
-
-const options_for_objects = [
-  'expiresIn',
-  'notBefore',
-  'noTimestamp',
-  'audience',
-  'issuer',
-  'subject',
-  'jwtid',
-];
+const { KeyObject } = require('crypto')
+const { normalizePrivateKey } = require('./lib/keyUtils');
+const {
+  validateSignOptions,
+  validateRegisteredClaims,
+  optionsToPayload,
+  optionsForNonObjectPayload,
+} = require('./lib/options/sign');
+const {
+  applySignTimestampClaims,
+  applySignOptionClaims,
+} = require('./lib/claims/sign');
 
 module.exports = function (payload, secretOrPrivateKey, options, callback) {
   if (typeof options === 'function') {
@@ -113,13 +46,9 @@ module.exports = function (payload, secretOrPrivateKey, options, callback) {
 
   if (secretOrPrivateKey != null && !(secretOrPrivateKey instanceof KeyObject)) {
     try {
-      secretOrPrivateKey = createPrivateKey(secretOrPrivateKey)
+      secretOrPrivateKey = normalizePrivateKey(secretOrPrivateKey);
     } catch (_) {
-      try {
-        secretOrPrivateKey = createSecretKey(typeof secretOrPrivateKey === 'string' ? Buffer.from(secretOrPrivateKey) : secretOrPrivateKey)
-      } catch (_) {
-        return failure(new Error('secretOrPrivateKey is not valid key material'));
-      }
+      return failure(new Error('secretOrPrivateKey is not valid key material'));
     }
   }
 
@@ -141,7 +70,7 @@ module.exports = function (payload, secretOrPrivateKey, options, callback) {
     return failure(new Error('payload is required'));
   } else if (isObjectPayload) {
     try {
-      validatePayload(payload);
+      validateRegisteredClaims(payload);
     }
     catch (error) {
       return failure(error);
@@ -150,7 +79,7 @@ module.exports = function (payload, secretOrPrivateKey, options, callback) {
       payload = Object.assign({},payload);
     }
   } else {
-    const invalid_options = options_for_objects.filter(function (opt) {
+    const invalid_options = optionsForNonObjectPayload.filter(function (opt) {
       return typeof options[opt] !== 'undefined';
     });
 
@@ -168,7 +97,7 @@ module.exports = function (payload, secretOrPrivateKey, options, callback) {
   }
 
   try {
-    validateOptions(options);
+    validateSignOptions(options);
   }
   catch (error) {
     return failure(error);
@@ -182,47 +111,12 @@ module.exports = function (payload, secretOrPrivateKey, options, callback) {
     }
   }
 
-  const timestamp = payload.iat || Math.floor(Date.now() / 1000);
-
-  if (options.noTimestamp) {
-    delete payload.iat;
-  } else if (isObjectPayload) {
-    payload.iat = timestamp;
+  try {
+    applySignTimestampClaims(payload, isObjectPayload, options, timespan);
+    applySignOptionClaims(payload, options, optionsToPayload);
+  } catch (err) {
+    return failure(err);
   }
-
-  if (typeof options.notBefore !== 'undefined') {
-    try {
-      payload.nbf = timespan(options.notBefore, timestamp);
-    }
-    catch (err) {
-      return failure(err);
-    }
-    if (typeof payload.nbf === 'undefined') {
-      return failure(new Error('"notBefore" should be a number of seconds or string representing a timespan eg: "1d", "20h", 60'));
-    }
-  }
-
-  if (typeof options.expiresIn !== 'undefined' && typeof payload === 'object') {
-    try {
-      payload.exp = timespan(options.expiresIn, timestamp);
-    }
-    catch (err) {
-      return failure(err);
-    }
-    if (typeof payload.exp === 'undefined') {
-      return failure(new Error('"expiresIn" should be a number of seconds or string representing a timespan eg: "1d", "20h", 60'));
-    }
-  }
-
-  Object.keys(options_to_payload).forEach(function (key) {
-    const claim = options_to_payload[key];
-    if (typeof options[key] !== 'undefined') {
-      if (typeof payload[claim] !== 'undefined') {
-        return failure(new Error('Bad "options.' + key + '" option. The payload already has an "' + claim + '" property.'));
-      }
-      payload[claim] = options[key];
-    }
-  });
 
   const encoding = options.encoding || 'utf8';
 
--- verify.js
+++ verify.js
@@ -1,22 +1,24 @@
 const JsonWebTokenError = require('./lib/JsonWebTokenError');
-const NotBeforeError = require('./lib/NotBeforeError');
-const TokenExpiredError = require('./lib/TokenExpiredError');
 const decode = require('./decode');
 const timespan = require('./lib/timespan');
 const validateAsymmetricKey = require('./lib/validateAsymmetricKey');
-const PS_SUPPORTED = require('./lib/psSupported');
 const jws = require('jws');
-const {KeyObject, createSecretKey, createPublicKey} = require("crypto");
-
-const PUB_KEY_ALGS = ['RS256', 'RS384', 'RS512'];
-const EC_KEY_ALGS = ['ES256', 'ES384', 'ES512'];
-const RSA_KEY_ALGS = ['RS256', 'RS384', 'RS512'];
-const HS_ALGS = ['HS256', 'HS384', 'HS512'];
-
-if (PS_SUPPORTED) {
-  PUB_KEY_ALGS.splice(PUB_KEY_ALGS.length, 0, 'PS256', 'PS384', 'PS512');
-  RSA_KEY_ALGS.splice(RSA_KEY_ALGS.length, 0, 'PS256', 'PS384', 'PS512');
-}
+const {KeyObject} = require("crypto");
+const { getVerifyDefaultAlgorithmsForKey } = require('./lib/algorithms');
+const { normalizePublicKeyOrSecret } = require('./lib/keyUtils');
+const { nowSeconds } = require('./lib/clock');
+const { validateVerifyOptions } = require('./lib/options/verify');
+const { validateSignaturePresenceAndKey } = require('./lib/policy/verifySignature');
+const {
+  validateNbf,
+  validateExp,
+  validateAudience,
+  validateIssuer,
+  validateSubject,
+  validateJwtId,
+  validateNonce,
+  validateMaxAge,
+} = require('./lib/claims/verify');
 
 module.exports = function (jwtString, secretOrPublicKey, options, callback) {
   if ((typeof options === 'function') && !callback) {
@@ -42,19 +44,13 @@ module.exports = function (jwtString, secretOrPublicKey, options, callback) {
     };
   }
 
-  if (options.clockTimestamp && typeof options.clockTimestamp !== 'number') {
-    return done(new JsonWebTokenError('clockTimestamp must be a number'));
+  try {
+    validateVerifyOptions(options);
+  } catch (e) {
+    return done(e);
   }
 
-  if (options.nonce !== undefined && (typeof options.nonce !== 'string' || options.nonce.trim() === '')) {
-    return done(new JsonWebTokenError('nonce must be a non-empty string'));
-  }
-
-  if (options.allowInvalidAsymmetricKeyTypes !== undefined && typeof options.allowInvalidAsymmetricKeyTypes !== 'boolean') {
-    return done(new JsonWebTokenError('allowInvalidAsymmetricKeyTypes must be a boolean'));
-  }
-
-  const clockTimestamp = options.clockTimestamp || Math.floor(Date.now() / 1000);
+  const clockTimestamp = nowSeconds(options.clockTimestamp);
 
   if (!jwtString){
     return done(new JsonWebTokenError('jwt must be provided'));
@@ -105,43 +101,23 @@ module.exports = function (jwtString, secretOrPublicKey, options, callback) {
 
     const hasSignature = parts[2].trim() !== '';
 
-    if (!hasSignature && secretOrPublicKey){
-      return done(new JsonWebTokenError('jwt signature is required'));
-    }
-
-    if (hasSignature && !secretOrPublicKey) {
-      return done(new JsonWebTokenError('secret or public key must be provided'));
-    }
-
-    if (!hasSignature && !options.algorithms) {
-      return done(new JsonWebTokenError('please specify "none" in "algorithms" to verify unsigned tokens'));
+    try {
+      validateSignaturePresenceAndKey(hasSignature, secretOrPublicKey, options.algorithms);
+    } catch (e) {
+      return done(e);
     }
 
     if (secretOrPublicKey != null && !(secretOrPublicKey instanceof KeyObject)) {
       try {
-        secretOrPublicKey = createPublicKey(secretOrPublicKey);
+        secretOrPublicKey = normalizePublicKeyOrSecret(secretOrPublicKey);
       } catch (_) {
-        try {
-          secretOrPublicKey = createSecretKey(typeof secretOrPublicKey === 'string' ? Buffer.from(secretOrPublicKey) : secretOrPublicKey);
-        } catch (_) {
-          return done(new JsonWebTokenError('secretOrPublicKey is not valid key material'))
-        }
+        return done(new JsonWebTokenError('secretOrPublicKey is not valid key material'))
       }
     }
 
-    if (!options.algorithms) {
-      if (secretOrPublicKey.type === 'secret') {
-        options.algorithms = HS_ALGS;
-      } else if (['rsa', 'rsa-pss'].includes(secretOrPublicKey.asymmetricKeyType)) {
-        options.algorithms = RSA_KEY_ALGS
-      } else if (secretOrPublicKey.asymmetricKeyType === 'ec') {
-        options.algorithms = EC_KEY_ALGS
-      } else {
-        options.algorithms = PUB_KEY_ALGS
-      }
-    }
+    const allowedAlgorithms = options.algorithms || getVerifyDefaultAlgorithmsForKey(secretOrPublicKey);
 
-    if (options.algorithms.indexOf(decodedToken.header.alg) === -1) {
+    if (allowedAlgorithms.indexOf(decodedToken.header.alg) === -1) {
       return done(new JsonWebTokenError('invalid algorithm'));
     }
 
@@ -173,79 +149,19 @@ module.exports = function (jwtString, secretOrPublicKey, options, callback) {
 
     const payload = decodedToken.payload;
 
-    if (typeof payload.nbf !== 'undefined' && !options.ignoreNotBefore) {
-      if (typeof payload.nbf !== 'number') {
-        return done(new JsonWebTokenError('invalid nbf value'));
-      }
-      if (payload.nbf > clockTimestamp + (options.clockTolerance || 0)) {
-        return done(new NotBeforeError('jwt not active', new Date(payload.nbf * 1000)));
-      }
-    }
+    const errors = [
+      validateNbf(payload, options, clockTimestamp),
+      validateExp(payload, options, clockTimestamp),
+      validateAudience(payload, options),
+      validateIssuer(payload, options),
+      validateSubject(payload, options),
+      validateJwtId(payload, options),
+      validateNonce(payload, options),
+      validateMaxAge(payload, options, clockTimestamp, timespan),
+    ].filter(Boolean);
 
-    if (typeof payload.exp !== 'undefined' && !options.ignoreExpiration) {
-      if (typeof payload.exp !== 'number') {
-        return done(new JsonWebTokenError('invalid exp value'));
-      }
-      if (clockTimestamp >= payload.exp + (options.clockTolerance || 0)) {
-        return done(new TokenExpiredError('jwt expired', new Date(payload.exp * 1000)));
-      }
-    }
-
-    if (options.audience) {
-      const audiences = Array.isArray(options.audience) ? options.audience : [options.audience];
-      const target = Array.isArray(payload.aud) ? payload.aud : [payload.aud];
-
-      const match = target.some(function (targetAudience) {
-        return audiences.some(function (audience) {
-          return audience instanceof RegExp ? audience.test(targetAudience) : audience === targetAudience;
-        });
-      });
-
-      if (!match) {
-        return done(new JsonWebTokenError('jwt audience invalid. expected: ' + audiences.join(' or ')));
-      }
-    }
-
-    if (options.issuer) {
-      const invalid_issuer =
-              (typeof options.issuer === 'string' && payload.iss !== options.issuer) ||
-              (Array.isArray(options.issuer) && options.issuer.indexOf(payload.iss) === -1);
-
-      if (invalid_issuer) {
-        return done(new JsonWebTokenError('jwt issuer invalid. expected: ' + options.issuer));
-      }
-    }
-
-    if (options.subject) {
-      if (payload.sub !== options.subject) {
-        return done(new JsonWebTokenError('jwt subject invalid. expected: ' + options.subject));
-      }
-    }
-
-    if (options.jwtid) {
-      if (payload.jti !== options.jwtid) {
-        return done(new JsonWebTokenError('jwt jwtid invalid. expected: ' + options.jwtid));
-      }
-    }
-
-    if (options.nonce) {
-      if (payload.nonce !== options.nonce) {
-        return done(new JsonWebTokenError('jwt nonce invalid. expected: ' + options.nonce));
-      }
-    }
-
-    if (options.maxAge) {
-      if (typeof payload.iat !== 'number') {
-        return done(new JsonWebTokenError('iat required when maxAge is specified'));
-      }
-
-      const maxAgeTimestamp = timespan(options.maxAge, payload.iat);
-      if (typeof maxAgeTimestamp === 'undefined') {
-        return done(new JsonWebTokenError('"maxAge" should be a number of seconds or string representing a timespan eg: "1d", "20h", 60'));
-      }
-      if (clockTimestamp >= maxAgeTimestamp + (options.clockTolerance || 0)) {
-        return done(new TokenExpiredError('maxAge exceeded', new Date(maxAgeTimestamp * 1000)));
-      }
+    if (errors.length > 0) {
+      return done(errors[0]);
     }
 
     if (options.complete === true) {
